e075c3c18ec489c8aad7b093bccfc1c34db75c13
conkobar <connorkleine@gmail.com>
2023-01-06 14:15:42 -0600

    task 1

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task2 = false;
+    const task = true;
+  }
+
+  return [task, task2];
+}
