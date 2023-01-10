d0f4771c2e5fd32e6e0bd3f79df905deebfd9c1f
conkobar <connorkleine@gmail.com>
2023-01-10 13:20:06 -0600

    task 6

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,12 +4,10 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((response) => {
       console.log(
-        `${response[0].body} ${response[1].firstName} ${response[1].lastName}`,
+        `${response[0].body} ${response[1].firstName} ${response[1].lastName}`
       );
     })
     .catch(() => {
-      console.log(
-        'Signup system offline',
-      );
+      console.log('Signup system offline');
     });
 }
