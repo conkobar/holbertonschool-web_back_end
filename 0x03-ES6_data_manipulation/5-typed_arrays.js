f27bf1d93c451729569aa13431eb75e515f087df
conkobar <connorkleine@gmail.com>
2023-01-16 21:14:21 -0600

    throw error

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,5 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position > length) throw new Error('Position outside range');
   const view = new DataView(new ArrayBuffer(length));
   view.setInt8(position, value);
   return view;
