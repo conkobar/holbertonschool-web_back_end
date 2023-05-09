d914009aa54058e47b4481de6b48ff282bd249a8
conkobar <connorkleine@gmail.com>
2023-05-09 14:39:45 -0500

    idk

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -12,14 +12,15 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-        wait_n should return the list of all the delays
-        (float values). The list of the delays should be in
-        ascending order without using sort() because of concurrency.
-    """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    wait_list = []
-    for task in asyncio.as_completed(tasks):
-        result = await task
-        wait_list.append(result)
-    return wait_list
+    """task_wait_n function declaration"""
+    delays = []
+    tasks = []
+    for i in range(n):
+        task = task_wait_random(max_delay)
+        tasks.append(task)
+
+    for task in tasks:
+        delay = await task
+        delays.append(delay)
+
+    return sorted(delays)
