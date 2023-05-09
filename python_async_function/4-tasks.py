18bf5be5e206208e19f8734dc01bc52dcb5367c1
conkobar <connorkleine@gmail.com>
2023-05-09 14:19:28 -0500

    tasks thru 4 i think; checker is giving me hell

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+    Take the code from wait_n and alter it into a
+    new function task_wait_n. The code is nearly identical
+    to wait_n except task_wait_random is being called.
+"""
+
+
+from typing import List
+import asyncio
+task_wait_random = __import__("3-tasks").wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+        wait_n should return the list of all the delays
+        (float values). The list of the delays should be in
+        ascending order without using sort() because of concurrency.
+    """
+    # make a list of the tasks
+    tasks = asyncio.as_completed([
+        asyncio.create_task(
+            task_wait_random(max_delay)
+        ) for _ in range(n)
+    ])
+    # create a list of delay times
+    wait_list = []
+    # loop thru the tasks and append to wait times
+    for task in tasks:
+        result = await task
+        wait_list.append(result)
+    # return wait times
+    return wait_list
