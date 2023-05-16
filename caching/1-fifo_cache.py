d5ece6f4f0c20c6e4b42a61747570052c4a30a77
conkobar <connorkleine@gmail.com>
2023-05-16 14:15:25 -0500

    task 1

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+    FIFO caching system inheriting from BaseCaching
+"""
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ first-in-first-out caching system inheriting from BaseCaching """
+
+    def __init__(self):
+        """ new init func with cache list """
+        super().__init__()
+        self.cache = []
+
+    def put(self, key, item):
+        """
+            assigns key: item to self.cache_data if they both exist
+            as long as the cache has not exceeded MAX_ITEMS
+        """
+        if key and item:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                popper = self.cache.pop(0)
+                print(f"DISCARD: {popper}")
+                del self.cache_data[popper]
+            self.cache.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ return value in cache_data linked to key """
+        return self.cache_data[key] if key in self.cache_data else None
