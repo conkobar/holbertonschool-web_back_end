245aa3de29aa3dff21432ec8699fcdc8436e38c7
conkobar <connorkleine@gmail.com>
2023-05-24 16:05:12 -0500

    task 4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -6,6 +6,7 @@ authentication with python3
 
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth():
@@ -13,7 +14,9 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ returns True if authenticated, else False"""
-        return False
+        if not path or not excluded_paths:
+            return True
+        return False if os.path.join(path, '') in excluded_paths else True
 
     def authorization_header(self, request=None) -> str:
         """ returns Flask request """
