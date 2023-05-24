55f067717b531bfffdee1e0a58a3936976ff7ef3
conkobar <connorkleine@gmail.com>
2023-05-24 10:04:38 -0500

    task 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -9,3 +9,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ returns a salted, hashed password as a byte string """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ checks a hashed_password against the real deal """
+    bcrypt.checkpw(password.encode("utf-8"), hashed_password)
