a5f8182bda1b5ca55493bd1849dfec18c0189f3f
conkobar <connorkleine@gmail.com>
2023-05-24 20:36:55 -0500

    task 6

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+basic authentication mechanism
+"""
+
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ basic auth class inheriting from Auth """
