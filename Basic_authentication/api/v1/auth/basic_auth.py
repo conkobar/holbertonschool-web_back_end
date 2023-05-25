b32aa07473f9bfebacb7b53ed67e2fc8ff21d7d9
conkobar <connorkleine@gmail.com>
2023-05-24 21:04:01 -0500

    pybetty

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,7 +12,9 @@ import base64
 class BasicAuth(Auth):
     """ basic auth class inheriting from Auth """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
         """
         returns the Base64 part of the Authorization
         header for a Basic Authentication
