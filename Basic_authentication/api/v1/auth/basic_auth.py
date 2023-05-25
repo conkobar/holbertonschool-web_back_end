c5aa14f462e0da28ea70394a98f1e4f562f3262d
conkobar <connorkleine@gmail.com>
2023-05-24 20:46:56 -0500

    task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,7 +5,20 @@ basic authentication mechanism
 
 
 from api.v1.auth.auth import Auth
+import re
+import base64
 
 
 class BasicAuth(Auth):
     """ basic auth class inheriting from Auth """
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        returns the Base64 part of the Authorization
+        header for a Basic Authentication
+        """
+        if not authorization_header \
+                or type(authorization_header) is not str \
+                or not re.search("^Basic ", authorization_header):
+            return None
+        return re.sub("^Basic ", "", authorization_header)
