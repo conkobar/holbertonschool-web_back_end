b4c6bf57f78cf4cd5831101bc406f56e0a27c72f
conkobar <connorkleine@gmail.com>
2023-05-31 22:13:00 -0500

    task 14

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -4,7 +4,7 @@ another flask app!
 """
 
 
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -42,6 +42,18 @@ def login():
         return response
     else:
         abort(401)
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    """ route for logouts """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id=session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect("/")
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
