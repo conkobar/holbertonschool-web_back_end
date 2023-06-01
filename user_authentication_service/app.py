f9d62462f222d0fa97bbec0ca6f824ecf6aedb53
conkobar <connorkleine@gmail.com>
2023-05-31 22:36:05 -0500

    task 17

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -68,5 +68,16 @@ def profile():
         abort(403)
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    """ route for pw reset token """
+    try:
+        email = request.form.get("email")
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
