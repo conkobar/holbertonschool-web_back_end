bb80f73abcbf8f421798811ba9c088a98fcb5120
conkobar <connorkleine@gmail.com>
2023-05-31 22:32:32 -0500

    task 16 another try

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -69,10 +69,10 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """ update user's password token """
-        user = self._db.find_user_by(email=email)
-        if not user:
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
             raise ValueError
-        # make new user id
-        reset_token = _generate_uuid()
-        self._db.update_user(user, reset_token=reset_token)
-        return reset_token
