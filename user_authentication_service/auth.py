4c73edff0e30f45b4f6f1cf29fa2135043c76602
conkobar <connorkleine@gmail.com>
2023-05-31 20:24:32 -0500

    task 5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -10,3 +10,17 @@ from bcrypt import hashpw, gensalt
 def _hash_password(password: str) -> str:
     """ salty hashbrowns """
     return hashpw(password.encode(), gensalt())
+
+class Auth:
+    """ authentication class """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ registers a new user """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
