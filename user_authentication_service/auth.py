310725dc6bb4a3bd4171a020d8865e90848fe24a
conkobar <connorkleine@gmail.com>
2023-05-31 21:28:11 -0500

    task 8

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,7 +4,7 @@ authentication service
 """
 
 
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 from user import User
@@ -28,3 +28,11 @@ class Auth:
             raise ValueError(f'User {email} already exists')
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ checks if login attempt is valid """
+        try:
+            user = self._db.find_user_by(email=email)
+            return checkpw(password.encode('utf-8'), user.hashed_password())
+        except NoResultFound:
+            return False
