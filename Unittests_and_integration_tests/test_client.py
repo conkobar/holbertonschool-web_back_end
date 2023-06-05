5bf6ef62170736b5211e447728dbd122bb9bed87
conkobar <connorkleine@gmail.com>
2023-06-05 14:12:37 -0500

    task 5

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -11,7 +11,7 @@ from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """ TestGithubOrgClient Class """
+    """ TestGithubOrgClient class """
 
     @parameterized.expand([
         ("google"),
@@ -19,11 +19,23 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch('client.get_json')
     def test_org(self, test_org_name, mock_get_json):
-        """ Test org method """
+        """ test org method """
         mock_get_json.return_value = {"payload": True}
         test_class = GithubOrgClient(test_org_name)
         self.assertEqual(test_class.org, mock_get_json.return_value)
         mock_get_json.assert_called_once()
+
+    @patch('client.get_json')
+    def test_public_repos_url(self, mock):
+        """ test _public_repos_url method """
+        test_class = GithubOrgClient("google")
+        mock.return_value = {
+            "repos_url": "https://api.github.com/orgs/google/repos"
+        }
+        self.assertEqual(
+            test_class._public_repos_url,
+            mock.return_value.get("repos_url")
+        )
 
 
 if __name__ == "__main__":
