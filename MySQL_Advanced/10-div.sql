fafd4b8ea6686f11a26e3649fc8e7c9de3c6a0e6
conkobar <connorkleine@gmail.com>
2023-06-22 14:30:17 -0500

    task 10

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,13 +1,15 @@
 -- creates a function that divides the first by the second number
 -- or returns 0 if second number is 0
-DELIMITER / /
+DELIMITER //
 
-CREATE FUNCTION SafeDiv (a INT, b INT) RETURNS FLOAT DETERMINISTIC BEGIN IF b = 0 THEN RETURN 0;
-
-END IF;
-
-RETURN a / b;
-
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+    RETURN a / b;
 END;
+//
 
-/ / DELIMITER;
+DELIMITER ;
