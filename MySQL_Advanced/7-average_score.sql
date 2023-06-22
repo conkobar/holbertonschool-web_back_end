22fa8678806990b27db2bb49e064176ddeae81f0
conkobar <connorkleine@gmail.com>
2023-06-22 14:03:35 -0500

    task 7 troubles

---
+++ MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,20 @@
+-- creates procedure to find student average score
+DELIMITER / / CREATE PROCEDURE ComputeAverageScoreForUser(IN USER_ID INT) BEGIN DECLARE avg_score FLOAT;
+
+SELECT
+  AVG(score) INTO avg_score
+FROM
+  corrections
+WHERE
+  corrections.user_id = user_id;
+
+UPDATE
+  users
+SET
+  average_score = avg_score
+WHERE
+  id = user_id;
+
+END;
+
+/ / DELIMITER;
