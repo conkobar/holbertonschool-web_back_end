9b5e0189fde42ff555e0f59ab21666b00fc0a4a2
conkobar <connorkleine@gmail.com>
2023-06-22 14:28:48 -0500

    task 10

--- MySQL_Advanced/9-index_name_score.sql
+++ MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,4 @@
--- creates index in table names & first letter of name & score
-CREATE INDEX idx_name_first_score ON names (name(1), score);
+-- creates index IN TABLE names & first letter of name & score
+
+CREATE INDEX idx_name_first_score
+ON names (name(1), score);
