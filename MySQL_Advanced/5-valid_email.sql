e04668d0fbae9abd0c3427b83221c0ecad04979a
conkobar <connorkleine@gmail.com>
2023-06-22 11:41:03 -0500

    task 5 need to update one at a time

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,12 +1,10 @@
--- creates trigger to reset valid_email when email is changed
-
 DELIMITER //
 
-CREATE TRIGGER RESET_ATTRIBUTE AFTER UPDATE ON USERS
+CREATE TRIGGER UPDATE_VALID_EMAIL AFTER UPDATE ON USERS
 FOR EACH ROW BEGIN
 	IF NEW.email != OLD.email THEN SET NEW.valid_email = 0;
 	END IF;
-	END //
+	END//
 
 
 DELIMITER ;
