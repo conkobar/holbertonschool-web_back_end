ed256a77de0cfd3aa2ce70b9c11a14208ac9345a
conkobar <connorkleine@gmail.com>
2023-06-26 13:31:21 -0500

    task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,13 @@
 // displays welcome message for new users
-const readline = require('readline');
+console.log('Welcome to Holberton School, what is your name>');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-console.log('Welcome to Holberton School, what is your name?');
-
-rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-  rl.close();
-});
-
-rl.on('close', () => {
-  console.log('This important software is now closing');
-});
+process.stdin
+  .on('readable', () => {
+    const name = process.stdin.read();
+    if (name) {
+      process.stdout.write(`Your name is ${name}`);
+    }
+  })
+  .on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
