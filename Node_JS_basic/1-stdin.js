6031f8874c78736d6ebc36508a8067f267b619d3
conkobar <connorkleine@gmail.com>
2023-06-26 13:45:54 -0500

    forgot the colon

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ process.stdin
   .on('readable', () => {
     const name = process.stdin.read();
     if (name) {
-      process.stdout.write(`Your name is ${name}`);
+      process.stdout.write(`Your name is: ${name}`);
     }
   })
   .on('end', () => {
