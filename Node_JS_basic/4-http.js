b9abe55883bac5fce192658ea27bd915c5a59d8c
conkobar <connorkleine@gmail.com>
2023-06-27 10:06:29 -0500

    task 4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+// creates small http server
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
