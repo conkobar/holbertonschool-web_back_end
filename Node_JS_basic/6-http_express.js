f90d56339e0345a965aeb1d37d80ce94b8509e6f
conkobar <connorkleine@gmail.com>
2023-06-27 11:30:26 -0500

    task 6

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,12 @@
+// creates a small http server using express
+const express = require('express');
+
+const app = express();
+
+app
+  .get('/', (req, res) => {
+    res.send('Hello Holberton School!');
+  })
+  .listen(1245);
+
+module.exports = app;
