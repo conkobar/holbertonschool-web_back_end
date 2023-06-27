f16c9f0d6b8fa2d5a18483d0323af23c4435cc46
conkobar <connorkleine@gmail.com>
2023-06-27 11:16:17 -0500

    stopped printing to console

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -3,44 +3,43 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(async (req, res) => {
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  }
-  if (req.url === '/students') {
-    res.write('This is the list of our students!\n');
-    await countStudents(process.argv[2])
-      .then((data) => {
-        const fields = Object.keys(data);
+    if (req.url === '/') {
+      res.end('Hello Holberton School!');
+    } else if (req.url === '/students') {
+      res.write('This is the list of our students\n');
 
-        const total = fields.reduce(
-          (acc, curr) => acc + data[curr].numStudents,
-          0
-        );
+      await countStudents(process.argv[2])
+        .then((data) => {
+          const fields = Object.keys(data);
 
-        res.write(`Number of students: ${total}\n`);
-
-        for (let i = 0; i < fields.length; i += 1) {
-          res.write(
-            `Number of students in ${fields[i]}: ${
-              data[fields[i]].numStudents
-            }. `
+          const total = fields.reduce(
+            (acc, curr) => acc + data[curr].numStudents,
+            0,
           );
-          res.write(`List: ${data[fields[i]].names.join(', ')}`);
 
-          if (i < fields.length - 1) {
-            res.write('\n');
+          res.write(`Number of students: ${total}\n`);
+
+          for (let i = 0; i < fields.length; i += 1) {
+            res.write(
+              `Number of students in ${fields[i]}: ${
+                data[fields[i]].numStudents
+              }. `,
+            );
+            res.write(`List: ${data[fields[i]].names.join(', ')}`);
+
+            if (i < fields.length - 1) {
+              res.write('\n');
+            }
           }
-        }
-      })
-      .catch((err) => {
-        res.write(err.message);
-      })
-      .finally(() => {
-        res.end();
-      });
-  }
-});
-
-app.listen(1245);
+        })
+        .catch((err) => {
+          res.write(err.message);
+        })
+        .finally(() => {
+          res.end();
+        });
+    }
+  })
+  .listen(1245);
 
 module.exports = app;
