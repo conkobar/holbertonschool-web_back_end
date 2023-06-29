298ac23e9399c1daa8ee22f75d8cef9301f3f3a0
conkobar <connorkleine@gmail.com>
2023-06-28 22:54:03 -0500

    task 12

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+provides stats about nginx logs in mongodb
+"""
+import pymongo
+
+
+def log_stats():
+    """ provides some nginx stats """
+    client = pymongo.MongoClient()
+    db = client.logs
+    logs = db.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    status = logs.count_documents({'method': 'GET', 'path': '/status'})
+    # print the number of logs
+    print(f"{logs.count_documents({})} logs")
+    # print the number of methods
+    print("Methods:")
+    for method in methods:
+        print(f"\tmethod {method}: {logs.count_documents({'method': method})}")
+    # print status check
+    print(f"{status} status check")
