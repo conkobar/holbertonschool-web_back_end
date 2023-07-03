7a4277f0e4785102be91837499bbc5e758184f1c
conkobar <connorkleine@gmail.com>
2023-07-03 14:25:13 -0500

    i am struggling here haha

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -9,11 +9,6 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(2.8, 4), 7);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     assert.strictEqual(calculateNumber(-1.6, -3.2), -5);
-  });
-
-  it('second number rounded', () => {
-    assert.strictEqual(calculateNumber(2, 3), 5);
-    assert.strictEqual(calculateNumber(1.3, 3), 4);
-    assert.strictEqual(calculateNumber(2.1, 3.5), 5);
+    assert.strictEqual(calculateNumber(1.3, 3.2), 4);
   });
 });
