6602bd20e0b24db19ef8cff6a317ad92b8c075c7
conkobar <connorkleine@gmail.com>
2023-07-03 13:45:04 -0500

    task 0 trying with different floats

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -6,7 +6,7 @@ describe('calculateNumber', () => {
   it('rounds a & b and returns their sum', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(2.8, 4), 7);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     assert.strictEqual(calculateNumber(-1.6, -3.2), -5);
   });
