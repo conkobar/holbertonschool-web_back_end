828369325eb3d1ea4809c919534963bba4c0640d
conkobar <connorkleine@gmail.com>
2023-07-05 15:51:34 -0500

    task 1

---
+++ unittests_in_js/1-calcul.js
@@ -0,0 +1,18 @@
+/* rounds a & b and returns their sum */
+function calculateNumber(type, a, b) {
+  // check that args are numbers
+  if (isNaN(a) || isNaN(b)) {
+    throw TypeError('Parameters must be numbers');
+  }
+  // return the result
+  if (type === 'SUM') return Math.round(a) + Math.round(b);
+  if (type === 'SUBTRACT') return Math.round(a) - Math.round(b);
+  if (type === 'DIVIDE') {
+    if (Math.round(a) === 0 || Math.round(b) === 0) {
+      throw new Error('Error');
+    }
+    return Math.round(a) / Math.round(b);
+  }
+}
+
+module.exports = calculateNumber;
