bccea0c26ca4242c77b7ee908ea6ba77bd46b891
conkobar <connorkleine@gmail.com>
2023-07-10 14:44:32 -0500

    bad payments function the whole time

--- unittests_in_js/3-payment.js
+++ unittests_in_js/3-payment.js
@@ -1,9 +1,10 @@
-// payments
+// a new payment function
 const Utils = require('./utils');
 
-function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const response = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${response}`);
-}
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const res = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${res}`);
+  return res;
+};
 
 module.exports = sendPaymentRequestToApi;
--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -1,4 +1,5 @@
-// test suite with sinon spy
+// Test suite for Sinon spy
+
 const sinon = require('sinon');
 const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
@@ -6,17 +7,16 @@ const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
   it('checks to see if the math used in sendPaymentRequestToApi(100, 20) is the same as Utils.calculateNumber("SUM", 100, 20)', () => {
-    // make the desired response
-    const res = sendPaymentRequestToApi(100, 20);
-    // create the spies
     const spyUtils = sinon.spy(Utils, 'calculateNumber');
-    const spyConsole = sinon.spy(console, 'log');
-    // watch for the spies
+    const consoleSpy = sinon.spy(console, 'log');
+
+    const res = sendPaymentRequestToApi(100, 20);
+
     expect(spyUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-    expect(spyConsole.calledWithExactly('The total is: 120')).to.be.true;
+    expect(consoleSpy.calledWithExactly('The total is: 120')).to.be.true;
     expect(Utils.calculateNumber('SUM', 100, 20)).to.equal(res);
-    // restore the spied methods
+
     spyUtils.restore();
-    spyConsole.restore();
+    consoleSpy.restore();
   });
 });
