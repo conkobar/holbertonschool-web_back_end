77d9b94e47652c0a6762137022a6864ebddaa2e4
conkobar <connorkleine@gmail.com>
2023-07-11 11:39:41 -0500

    task 8 but it says address in use

---
+++ unittests_in_js/8-api/api.test.js
@@ -0,0 +1,15 @@
+// test suite for index
+const app = require('./api');
+const { expect } = require('chai');
+const request = require('request');
+
+describe('Index page', () => {
+  it('should return correct status code and result', (done) => {
+    request('http://localhost:7865', (err, res, text) => {
+      expect(err).to.be.null;
+      expect(res.statusCode).to.equal(200);
+      expect(text).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
