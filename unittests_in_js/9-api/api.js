ebec18b6442bac3562fa088572c883b16b693eb1
conkobar <connorkleine@gmail.com>
2023-07-11 12:06:41 -0500

    task 9

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -6,6 +6,14 @@ app
   .get('/', (req, res) => {
     res.send('Welcome to the payment system');
   })
+  .get('/cart/:id', (req, res) => {
+    const id = req.params.id;
+    if (isNaN(id)) {
+      res.status(404).send('id must be a number');
+    } else {
+      res.send(`Payment methods for cart ${id}`);
+    }
+  })
   .listen(7865, () => {
     console.log('API available on localhost port 7865');
   });
---
+++ unittests_in_js/9-api/api.test.js
@@ -0,0 +1,30 @@
+// test suite for index
+const { expect } = require('chai');
+const request = require('request');
+
+describe('Index page', () => {
+  it('should return correct status code and result for /', (done) => {
+    request('http://localhost:7865', (err, res, text) => {
+      expect(err).to.be.null;
+      expect(res.statusCode).to.equal(200);
+      expect(text).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('returns correctly for /cart/:id when id is number', (done) => {
+    request('http://localhost:7865/cart/69', (err, res, text) => {
+      expect(err).to.be.null;
+      expect(res.statusCode).to.equal(200);
+      expect(text).to.equal('Payment methods for cart 69');
+      done();
+    });
+  });
+
+  it('returns correctly for /cart/:id when id is NaN', (done) => {
+    request('http://localhost:7865/cart/nan', (err, res, text) => {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
