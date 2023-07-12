bf4280f5cf383dea0119577c4639b6dce85af914
conkobar <connorkleine@gmail.com>
2023-07-12 13:40:50 -0500

    task 4

---
+++ queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,25 @@
+// Use client to store hash values
+
+const redis = require('redis');
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+// Store the hash value
+client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+client.hset('HolbertonSchools', 'New York', 20, redis.print);
+client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+
+// Display the hash
+client.hgetall('HolbertonSchools', (err, res) => {
+  console.log(res);
+});
