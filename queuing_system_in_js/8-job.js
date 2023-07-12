1f1212f33d93f2de116c21c2bfcd6ad95a4352fc
conkobar <connorkleine@gmail.com>
2023-07-12 14:05:14 -0500

    task 10

---
+++ queuing_system_in_js/8-job.js
@@ -0,0 +1,29 @@
+// function to create jobs
+
+const createPushNotificationsJobs = (jobs, queue) => {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+
+  for (const job of jobs) {
+    const newJob = queue.create('push_notification_code_3', job).save();
+
+    try {
+      newJob
+        .on('enqueue', () => {
+          console.log(`Notification job created: ${newJob.id}`);
+        })
+        .on('complete', () => {
+          console.log(`Notification job ${newJob.id} completed`);
+        })
+        .on('failed', (err) => {
+          console.log(`Notification job ${newJob.id} failed: ${err}`);
+        })
+        .on('progress', (progress) => {
+          console.log(`Notification job ${newJob.id} ${progress}% complete`);
+        });
+    } catch (err) {}
+  }
+};
+
+module.exports = createPushNotificationsJobs;
