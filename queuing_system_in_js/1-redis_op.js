547ce9540b23dffe54b68ae740550a4eaba87722
conkobar <connorkleine@gmail.com>
2023-07-12 13:37:56 -0500

    task 2

---
+++ queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,28 @@
+// Connect Redis server to Node and log a status msg
+
+// require('@babel/register');
+
+const redis = require('redis');
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, redis.print);
+};
+
+const displaySchoolValue = (schoolName) => {
+  client.get(schoolName, (err, res) => {
+    console.log(res);
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
---
+++ queuing_system_in_js/README.md
@@ -0,0 +1,6 @@
+# [<center>Queuing System in JS - 2375</center>](https://intranet.hbtn.io/projects/2375)
+ ---
+ ### <center>Description</center> 
+ ##### &emsp; Working with redis in js.
+ ---
+ [<center>Buzz</center>](http://github.com/conkobar)
